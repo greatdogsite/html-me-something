dd88e03bbcfa8ba9601c56d428a72e9c6f2eb82c
Paul <greatdogsite@gmail.com>
2023-02-13 00:32:05 -0600

    updated html, css and js

---
+++ script.js
@@ -0,0 +1,14 @@
+function init() {
+    //let question= confirm("Do you want my version?");
+    swal("Do you want my version?", {buttons: ["yes", "no"]})
+      .then((value) => {
+        // console.log(value);
+        if (value===null){
+            document.body.style.margin="auto";
+            document.getElementById("mainHeading").style.color="black";
+            document.getElementsByClassName("funParagraph")[0].style.color="black";
+        }
+      });
+  }
+  
+  window.addEventListener("load", init);
